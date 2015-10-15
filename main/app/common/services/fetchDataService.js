894eef0bd755f540c91dfd5a87f5573b6ab2e37d
alizarion <selim.bensenouci@gmail.com>
2015-10-15 21:22:33 +0200

    Create fetchDataService.js

---
+++ main/app/common/services/fetchDataService.js
@@ -0,0 +1,43 @@
+
+'use strict';
+angular.module('share.ws.demo')
+    .factory('FetchDataService',function() {
+        function _extractTraduction(data){
+            var i = 0;
+            var trads ={};
+            angular.forEach(data.header.columnsHeader,function(column){
+                trads[column.code] = data.documents[0].fields[i].translation;
+                i++
+            });
+            return trads;
+        }
+
+
+
+
+        function _extractValues(data){
+            console.log(data);
+            var documents = [];
+            data.documents.forEach(function(document){
+                var newDocument =  {};
+                newDocument.docIdxGed = document.docIdxGed;
+                var i = 0;
+                angular.forEach(document.fields,function(field){
+                    newDocument[data.header.columnsHeader[i].code] = field.value;
+                    i++;
+                });
+                documents.push(newDocument);
+            });
+            return documents;
+        }
+
+
+
+
+        return {
+            extractTraduction:_extractTraduction,
+            extractValues:_extractValues
+        }
+
+
+    });
